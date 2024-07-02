75f190bd72231ffe518f655d9e53f6454e6bfb13
lucas vacca fernandez <lucasvf4379@gmail.com>
2024-07-02 11:56:35 +0200

    carrito de compra rama carritoCompra

---
+++ index.js
@@ -0,0 +1,237 @@
+
+
+const botonCarrito = document.getElementById("carrito-compra")
+
+botonCarrito.addEventListener("click", (e)=>{
+        carritoDOM.classList.toggle("carrito-none")
+        e.preventDefault()
+        
+})
+
+const carritoDOM = document.getElementById("contenedor-carrito")
+
+
+
+
+let carritoArray =JSON.parse(localStorage.getItem("carrito")) || []
+
+function agregadoraDeEventos (evento, clase, eventoString){
+
+    const nodos = document.getElementsByClassName(clase)
+    const ArrayNodos = Array.from(nodos)
+
+    ArrayNodos.forEach(el =>{
+        el.addEventListener(eventoString, evento)
+    })
+}
+
+function actualizadora (){
+    mostrarCarrito()
+    agregadoraDeEventos(eliminarProducto, "eliminar-producto", "click")
+}
+
+
+// numero arriba del carrito
+
+let numCarrito = document.getElementById("carrito-compra")
+
+function numeroArribaCarrito (cantidad){
+
+        numCarrito.innerHTML += `<p class="num-carrito">${cantidad}</p>`
+        
+}
+
+//muestra el array en el carrito 
+
+function mostrarCarrito (){
+    if(!carritoArray){
+        return
+    }
+    carritoDOM.innerHTML = ""
+    carritoArray.forEach(el => {
+        carritoDOM.innerHTML += `<div class= "caja-producto">
+    <div class="img-carrito">
+       ${el.img}
+    </div>
+    <h4> ${el.titulo}</h4>
+       <p>${el.precio}</p>
+       <div class="caja-botones-productos"> 
+       <button class="eliminar-producto">-</button>
+                <p>${el.cantidad}</p>
+       <button class="agregar-producto">+</button>
+       </div>
+     
+       
+       </div>`
+        
+    })
+        let total = carritoArray.reduce((acumulador, i) => {
+            return acumulador + parseInt(i.precio) * parseInt(i.cantidad) 
+        },  0)
+        carritoDOM.innerHTML += ` <div><p class= "total-carrito"> Total : ${total}</p></div>`
+        carritoDOM.innerHTML += ` <div>
+                                        <button class="terminar-compra">Terminar compra</button>
+                                        <button class="limpiar-carrito">Limpiar carrito</button>
+                                    </div>`
+
+        agregadoraDeEventos(eliminarProducto, "eliminar-producto", "click")
+        agregadoraDeEventos(botonAgregarProducto, "agregar-producto", "click") 
+        agregadoraDeEventos(mostrarCompra, "terminar-compra", "click")
+        agregadoraDeEventos(terminarCarrito, "terminar-compra", "click")
+        agregadoraDeEventos(limpiarCarrito, "limpiar-carrito", "click")
+       
+
+
+        //numero en carrito
+        let cantidadCarrito = carritoArray.reduce((acumulador, i) => {
+            return acumulador + parseInt(i.cantidad) //* parseInt(i.cantidad) 
+        },  0)
+
+        numeroArribaCarrito(cantidadCarrito)
+
+}
+
+//mostrar compra 
+
+let carritoFlotante = document.getElementById("carrito-flotante")
+
+function mostrarCompra  (){
+    
+    let compraFinal = carritoArray.map((el) => {
+        return {
+            titulo : el.titulo,
+            precio: el.precio,
+            img: el.img
+        }
+    }
+      
+    )
+    console.log(compraFinal)
+if((compraFinal) ){
+    compraFinal.forEach(el => {
+        carritoFlotante.innerHTML += `
+          <div class="boton-carrito-final">
+           
+            <button ">x</button>
+             <h4>Gracias por confiar en nosotros</h4>
+        </div>
+        <div class=" cont-carrito-final">
+           <div class="img-carrito">
+                ${el.img}
+           </div>
+        <div>
+            <h5> ${el.titulo}</h5>
+            <p>${el.precio}</p>
+        </div>
+        </div>
+     
+       `})
+       carritoFlotante.classList.toggle("carrito-none")
+}else{
+    
+}
+       
+}
+
+// terminar compra 
+function terminarCarrito (){
+    //alert("Compra realizada ")
+    localStorage.clear()
+    carritoArray = []
+    mostrarCarrito()
+}
+
+function limpiarCarrito (){
+    document.getElementsByClassName("limpiar-carrito")
+    localStorage.clear()
+    carritoArray = []
+    mostrarCarrito()
+
+}
+
+
+
+// agregando array de procuctos al carrito 
+
+function agregarCarrito (e){
+   // console.dir(e)
+    let titulo = (e.target.parentElement.children[0].innerText)
+    let precio = (e.target.parentElement.children[1].innerText)
+    let img = (e.target.parentElement.parentElement.children[0].children[0].children[0].outerHTML)
+    
+
+    if(carritoArray.some ((el)=> {
+        return el.titulo == titulo 
+    })){
+        let titulos = carritoArray.map(el => el.titulo)
+        let indice = titulos.indexOf(titulo)
+       
+        
+        carritoArray[indice].cantidad ++ 
+        
+    }else{
+        carritoArray.push({
+
+            titulo,
+            precio,
+            cantidad: 1,
+            img
+        })
+    }
+
+   // alert("Producto agregado ")
+    localStorage.setItem("carrito", JSON.stringify(carritoArray))
+    actualizadora ()
+}
+
+function botonAgregarProducto (e){
+    let titulo =(e.target.parentElement.parentElement.children[1].innerText) //traemos el titulo de la caja padre 
+    let titulos = carritoArray.map(el => el.titulo) 
+    
+    let indice = titulos.indexOf(titulo)
+    if(carritoArray[indice].cantidad >= 1){
+
+        carritoArray[indice].cantidad ++
+
+    }
+    mostrarCarrito()
+    localStorage.setItem("carrito", JSON.stringify(carritoArray))
+}
+
+function eliminarProducto (e){
+    console.dir(e.target.parentElement.parentElement)
+    let titulo =(e.target.parentElement.parentElement.children[1].innerText) //traemos el titulo de la caja padre 
+    let titulos = carritoArray.map(el => el.titulo) 
+    
+        let indice = titulos.indexOf(titulo)
+        
+    if(carritoArray[indice].cantidad <= 1){
+
+        carritoArray.splice(indice, 1)
+
+    }else{
+
+       carritoArray[indice].cantidad --
+    }
+
+
+    mostrarCarrito()
+}
+
+agregadoraDeEventos(agregarCarrito, "boton-carrito", "click")
+
+
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+    mostrarCarrito()
+})
+
+
+
+
+//  //console.log(carritoArray)
+
+
+
+// numeroArribaCarrito()
